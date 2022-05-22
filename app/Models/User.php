1c4bb34284b3da2cc37bb1431de2877d8a89ffaf
Lam Pham <phamkhaclam90@gmail.com>
2022-05-22 13:46:52 +0700

    Add tcg/voyager package

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use TCG\Voyager\Models\User as BaseUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends BaseUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
